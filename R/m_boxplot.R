ce8ec2b6acf2fd7ba19ada89a501cef346c78a8e
sebmader <sebastianmader@gmx.net>
2019-11-04 17:54:12 +0100

    added axis labels to parameters of boxplot function

--- R/m_boxplot.R
+++ R/m_boxplot.R
@@ -1,24 +1,38 @@
 #' @title Function for simple boxplot
-#' for statistical analysis
+#'   for statistical analysis
 #' @name m_boxplot
-#' @param x Variable to use for x axis
-#' @param y Variable to use for y axis
-#' @param data Data set to use
+#' @param x factor vector for catigorical variable on x axis
+#' @param y vector for response variable on y axis
+#' @param labs vector of two elements for axis labels
+#'   (c("x label", "y label))
 #' @import ggplot2
 #' @export
 
 # require(assertthat)
 # require(ggplot2)
 
-m_boxplot <- function(x, y, data) {
+m_boxplot <- function(x, y, labs = c("","")) {
 
-  assertthat::assert_that(is.data.frame(data))
-  assertthat::assert_that(is.data.frame(x))
-  assertthat::assert_that(is.data.frame(y))
+  assertthat::assert_that(is.factor(x))
+  assertthat::assert_that(is.vector(y))
+  assertthat::are_equal(length(x), length(y))
+  assertthat::assert_that(is.vector(labs))
+  assertthat::assert_that(length(labs) == 2)
 
-  ggplot(data = data, mapping = aes(x = x, y = y))+
+  # display sample sizes in variable names for each category
+  lvls <- levels(x)
+  names_with_N <- vector(mode = "character", length = length(lvls))
+  for(pos in 1:length(lvls)) {
+    name = lvls[pos]
+    n <- length(x[which(x == name)])
+    names_with_N[pos] <- paste0(name, " (", "n = ", n, ")")
+  }
+
+  ggplot(mapping = aes(x = x, y = y))+
     geom_boxplot()+
+    scale_x_discrete(labels = names_with_N)+
     theme_bw()+
-    geom_point(size = 1, position = position_jitter(width = 0.05), color = "blue")
+    geom_point(size = 1, position = position_jitter(width = 0.05), color = "blue")+
+    labs(x = labs[1], y = labs[2])
 
 }
